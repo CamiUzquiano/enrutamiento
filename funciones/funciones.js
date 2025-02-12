626de1a5d33b9e31215a7b1b150eace76d941db3
Camila Uzquiano <camila.uzquiano@ucb.edu.bo>
2025-02-12 11:30:12 -0400

    Cambios con Javascript

---
+++ funciones/funciones.js
@@ -0,0 +1,4 @@
+//este es el nivel 3 de vincular funciones de javascript a html, vincula documentos
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("El DOM ha sido cargado");
+})
